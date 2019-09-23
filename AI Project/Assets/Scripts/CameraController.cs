b61a04b77f98b0d4d93ec3d7d0af27886cf6433f
GerardClotet <36155703+GerardClotet@users.noreply.github.com>
2019-09-23 17:20:58 +0200

    tutorial 3/8

---
+++ AI Project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset; /*curent transform position of the camera & subtract the transform position 
+    of the player to find the difference between the 2*/
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
