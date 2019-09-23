5fa704b1ad70e5069d77809563603ec846dfd3ae
GerardClotet <36155703+GerardClotet@users.noreply.github.com>
2019-09-23 16:34:45 +0200

    error on cs script

---
+++ AI Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private RigidBody rb;
+    void Start() //called in the first frame the  script is active
+    {
+        rb = GetComponent<RigidBody>();
+    }
+   //void Update() //called before rendering a FRAME
+   // {
+        
+   // }
+
+    void FixedUpdate() //called before performing any phisics calculation
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement);
+    }
+}
