80615a293ba6986928fb4956670a02428ecd5fd9
GerardClotet <36155703+GerardClotet@users.noreply.github.com>
2019-11-02 18:26:22 +0100

    animations handout

---
+++ handout/Handout9/Animations/Assets/DudeController.cs
@@ -0,0 +1,66 @@
+﻿//Set up a new Boolean parameter in the Unity Animator and name it, in this case “Jump”.
+//Set up transitions between each state that the animation could follow. For example, the player could be running or idle before they jump, so both would need transitions into the animation.
+//If the “Jump” boolean is set to true at any point, the m_Animator plays the animation. However, if it is ever set to false, the animation would return to the appropriate state (“Idle”).
+//This script enables and disables this boolean in this case by listening for the mouse click or a tap of the screen.
+
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DudeController : MonoBehaviour
+{
+    //Fetch the Animator
+    NavMeshAgent agent;
+    Animator m_Animator;
+    // Use this for deciding if the GameObject can jump or not
+    bool stop;
+    Transform target;
+
+    void Start()
+    {
+        //This gets the Animator, which should be attached to the GameObject you are intending to animate.
+        m_Animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        // The GameObject cannot jump
+        target = FindObjectOfType<MoveToMouse>().transform;
+        stop = false;
+    }
+
+    void Update()
+    {
+        Vector3 trgpos = target.position;
+        agent.SetDestination(trgpos);
+        //Click the mouse or tap the screen to change the animation
+
+
+        Vector3 anim_values = transform.InverseTransformDirection(agent.desiredVelocity);
+
+        if(anim_values != Vector3.zero)
+        {
+            Debug.Log(anim_values);
+            m_Animator.SetFloat("vel_x", anim_values.x);
+            m_Animator.SetFloat("vel_y", anim_values.z);
+            m_Animator.SetBool("movement", true);
+        }
+        else m_Animator.SetBool("movement", false);
+
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    stop = true;
+        //    Debug.Log("pressed 1");
+
+        //}
+        ////Otherwise the GameObject cannot jump.
+        //else if (Input.GetMouseButtonDown(1))
+        //{
+        //    stop = false;
+        //    Debug.Log("pressed 1");
+        //}
+        ////If the GameObject is not jumping, send that the Boolean “Jump” is false to the Animator. The jump animation does not play.
+        //if (stop == false)
+        //    m_Animator.SetBool("movement", false);
+
+        ////The GameObject is jumping, so send the Boolean as enabled to the Animator. The jump animation plays.
+        //if (stop == true)
+        //    m_Animator.SetBool("movement", true);
+    }
+}
---
+++ handout/Handout9/Animations/Assets/MoveToMouse.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MoveToMouse : MonoBehaviour
+{
+    public LayerMask mask;
+    public int mouse_button = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(mouse_button))
+        {
+            RaycastHit hit;
+            Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(r, out hit, 10000.0f, mask) == true)
+            {
+                transform.position = hit.point;
+            }
+        }
+    }
+}
